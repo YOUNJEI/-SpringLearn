8b727234d6bfa5a7f2fe74a692e05d6a40becc67
YOUNJEI <kyj951211@gmail.com>
2022-06-22 18:56:05 +0900

    update : lombok

---
+++ spring-learn/src/main/java/com/jojoldu/book/springlearn/web/dto/HelloResponseDto.java
@@ -0,0 +1,4 @@
+package com.jojoldu.book.springlearn.web.dto;
+
+public class HelloResponseDto {
+}
--- spring-learn/src/test/java/com/jojoldu/book/springlearn/web/HelloControllerTest.java
+++ spring-learn/src/test/java/com/jojoldu/book/springlearn/web/HelloControllerTest.java
@@ -1,4 +1,33 @@
 package com.jojoldu.book.springlearn.web;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
 }
