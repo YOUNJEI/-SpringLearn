74f2110707b5459992e41d923f80c9fa0921ed5f
YOUNJEI <kyj951211@gmail.com>
2022-07-04 23:41:15 +0900

    update : @LoginUser 구현

--- spring-learn/src/main/java/com/jojoldu/book/springlearn/web/IndexController.java
+++ spring-learn/src/main/java/com/jojoldu/book/springlearn/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springlearn.web;
 
+import com.jojoldu.book.springlearn.config.auth.LoginUser;
 import com.jojoldu.book.springlearn.config.auth.dto.SessionUser;
 import com.jojoldu.book.springlearn.domain.posts.PostsRepository;
 import com.jojoldu.book.springlearn.service.posts.PostsService;
@@ -19,10 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.
-                                        getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
